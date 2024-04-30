4839b5c4ed2dbc445d46413a36900250f829a15d
coffeebeats <108542400+coffeebeats@users.noreply.github.com>
2024-04-29 21:58:49 -0700

    chore: move `Main.tscn` under `core/framework`

---
+++ core/framework/Main.gd
@@ -0,0 +1,49 @@
+##
+## Class 'Main' is the top-level 'Node', responsible for orchestrating all
+## systems and scenes.
+##
+
+extends Node
+
+# -- DEPENDENCIES -------------------------------------------------------------------- #
+
+# -- DEFINITIONS --------------------------------------------------------------------- #
+
+# -- SIGNALS ------------------------------------------------------------------------- #
+
+# -- CONFIGURATION ------------------------------------------------------------------- #
+
+# -- INITIALIZATION ------------------------------------------------------------------ #
+
+# -- PUBLIC METHODS ------------------------------------------------------------------ #
+
+# -- ENGINE METHODS (OVERRIDES) ------------------------------------------------------ #
+
+#func _enter_tree() -> void:
+#	pass
+
+#func _exit_tree() -> void:
+#	pass
+
+#func _notification(what) -> void:
+#	pass
+
+#func _physics_process(delta: float) -> void:
+#	pass
+
+#func _process(delta: float) -> void:
+#	pass
+
+#func _ready() -> void:
+#	pass
+
+#func _unhandled_input(event: InputEvent) -> void:
+#	pass
+
+# -- PRIVATE METHODS (OVERRIDES) ----------------------------------------------------- #
+
+# -- PRIVATE METHODS ----------------------------------------------------------------- #
+
+# -- SIGNAL HANDLERS ----------------------------------------------------------------- #
+
+# -- SETTERS/GETTERS ----------------------------------------------------------------- #
