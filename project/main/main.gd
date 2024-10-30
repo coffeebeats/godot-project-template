a879dc8f8370b88330dd089371f4750bdc5534f6
coffeebeats <108542400+coffeebeats@users.noreply.github.com>
2024-10-29 23:53:50 -0700

    fix(main): use the shared `Lifecycle` library for shutdowns

--- project/main/main.gd
+++ project/main/main.gd
@@ -5,20 +5,24 @@
 
 extends Node
 
+# -- DEPENDENCIES -------------------------------------------------------------------- #
+
+const Lifecycle := preload("res://project/main/lifecycle.gd")
 
 # -- ENGINE METHODS (OVERRIDES) ------------------------------------------------------ #
 
+
 func _notification(what):
-    # Prior to quitting, propagate the quit request to all nodes in the scene tree. This
-    # allows for graceful shutdown. See
-    #   https://docs.godotengine.org/en/stable/tutorials/inputs/handling_quit_requests.html#handling-the-notification.
-    match what:
-        NOTIFICATION_WM_CLOSE_REQUEST, NOTIFICATION_WM_GO_BACK_REQUEST:
-            get_tree().root.propagate_notification(NOTIFICATION_WM_CLOSE_REQUEST)
-            get_tree().quit()
+	# Prior to quitting, propagate the quit request to all nodes in the scene tree. This
+	# allows for graceful shutdown. See
+	#   https://docs.godotengine.org/en/stable/tutorials/inputs/handling_quit_requests.html#handling-the-notification.
+	match what:
+		NOTIFICATION_WM_CLOSE_REQUEST, NOTIFICATION_WM_GO_BACK_REQUEST:
+			Lifecycle.shutdown(self)
+
 
 func _enter_tree() -> void:
-    # Prevent the game from automatically exiting when requested by the OS. Instead,
-    # first propagate a quit signal to all nodes, allowing for a graceful shutdown. For
-    # reference, see https://docs.godotengine.org/en/stable/tutorials/inputs/handling_quit_requests.html#handling-the-notification.
-    get_tree().set_auto_accept_quit(false)
+	# Prevent the game from automatically exiting when requested by the OS. Instead,
+	# first propagate a quit signal to all nodes, allowing for a graceful shutdown. For
+	# reference, see https://docs.godotengine.org/en/stable/tutorials/inputs/handling_quit_requests.html#handling-the-notification.
+	get_tree().set_auto_accept_quit(false)
--- project/main/menu/main_menu.gd
+++ project/main/menu/main_menu.gd
@@ -6,19 +6,22 @@
 
 extends Control
 
+# -- DEPENDENCIES -------------------------------------------------------------------- #
+
+const Lifecycle := preload("res://project/main/lifecycle.gd")
+
 # -- ENGINE METHODS (OVERRIDES) ------------------------------------------------------ #
 
-func _ready() -> void:
-    var err: int = %Quit.pressed.connect(_on_Quit_pressed)
-    assert(err == OK, "failed to connect to signal")
 
-    %Play.grab_focus()
+func _ready() -> void:
+	var err: int = %Quit.pressed.connect(_on_Quit_pressed)
+	assert(err == OK, "failed to connect to signal")
+
+	%Play.grab_focus()
 
 
 # -- SIGNAL HANDLERS ----------------------------------------------------------------- #
 
+
 func _on_Quit_pressed():
-    # Propagate the quit request to all nodes in the scene, then exit. See
-    # https://docs.godotengine.org/en/stable/tutorials/inputs/handling_quit_requests.html#sending-your-own-quit-notification.
-    get_tree().root.propagate_notification(NOTIFICATION_WM_CLOSE_REQUEST)
-    get_tree().quit()
+	Lifecycle.shutdown(self)
