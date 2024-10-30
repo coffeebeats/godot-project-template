5e09aa1a6e4dab8d2d1c60c1bb95de7309db39fa
coffeebeats <108542400+coffeebeats@users.noreply.github.com>
2024-10-30 08:41:23 -0700

    fix(main): move `Lifecycle` to an autoload

--- project/main/lifecycle.gd
+++ project/main/lifecycle.gd
@@ -1,31 +1,31 @@
 ##
 ## Lifecycle
 ##
-## A shared library for coordinating lifecycle events, like application shutdown.
-##
-## NOTE: This 'Object' should *not* be instanced and/or added to the 'SceneTree'. It is a
-## "static" library that can be imported at compile-time using 'preload'.
+## An autoloaded node for coordinating lifecycle events, like application shutdown.
 ##
 
-extends Object
+extends Node
+
+# -- SIGNALS ------------------------------------------------------------------------- #
+
+signal shutdown_requested(exit_code: int)
 
 # -- INITIALIZATION ------------------------------------------------------------------ #
 
 ## _is_shutdown_requested tracks whether a shutdown request has been issued. This helps
 ## prevent recursively invoking shutdown handlers.
-static var _is_shutdown_requested: bool = false
+var _is_shutdown_requested: bool = false
 
 ## _lifecycle_mu is a 'Mutex' guarding lifecycle state. This enables multiple threads to
 ## safely use the methods here.
-static var _lifecycle_mu: Mutex = Mutex.new()
+var _lifecycle_mu: Mutex = Mutex.new()
 
 # -- PUBLIC METHODS ------------------------------------------------------------------ #
 
 
 ## shutdown gracefully shuts down the application, allowing all nodes in the scene the
 ## chance to respond to the shutdown notification.
-static func shutdown(node: Node, exit_code: int = 0) -> void:
-	assert(node is Node, "invalid argument: node")
+func shutdown(exit_code: int = 0) -> void:
 	assert(exit_code >= 0, "invalid argument: must be >= 0")
 
 	_lifecycle_mu.lock()
@@ -40,10 +40,13 @@ static func shutdown(node: Node, exit_code: int = 0) -> void:
 	if should_exit:
 		return
 
-	# Propagate the quit request to all nodes in the scene, then exit. See
+	# First, emit the shutdown signal so any listeners can gracefully handle it.
+	shutdown_requested.emit(exit_code)
+
+	# Next, propagate the quit request to all nodes in the scene, then exit. See
 	# https://docs.godotengine.org/en/stable/tutorials/inputs/handling_quit_requests.html#sending-your-own-quit-notification. # gdlint:ignore=max-line-length
-	node.get_tree().root.propagate_notification(Node.NOTIFICATION_WM_CLOSE_REQUEST)
-	node.get_tree().quit(exit_code)
+	get_tree().root.propagate_notification(NOTIFICATION_WM_CLOSE_REQUEST)
+	get_tree().quit(exit_code)
 
 
 # -- ENGINE METHODS (OVERRIDES) ------------------------------------------------------ #
