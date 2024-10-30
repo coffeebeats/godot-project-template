3fb51313cbc38b7c0277f0cdf44fc149d8f9e6b6
coffeebeats <108542400+coffeebeats@users.noreply.github.com>
2024-10-30 08:33:28 -0700

    feat(main-menu): configure basic main menu; better handle quit requests (#133)
    * feat(main): configure default fonts
    
    * feat(main): configure a default theme
    
    * fix(main): update reference resolution to most common value from steam survey
    
    * fix: update `version.gd` methods to be static
    
    * feat(main): gracefully shut down after exit requests
    
    * feat(main): set up basic main menu with quit behavior
    
    * fix(main): add a `Lifecycle` library for coordinating shutdown
    
    * fix(main): use the shared `Lifecycle` library for shutdowns
    
    * chore: fix formatting (on behalf of 'coffeebeats')
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- project/main/menu/main_menu.gd
+++ project/main/menu/main_menu.gd
@@ -5,3 +5,23 @@
 ##
 
 extends Control
+
+# -- DEPENDENCIES -------------------------------------------------------------------- #
+
+const Lifecycle := preload("res://project/main/lifecycle.gd")
+
+# -- ENGINE METHODS (OVERRIDES) ------------------------------------------------------ #
+
+
+func _ready() -> void:
+	var err: int = %Quit.pressed.connect(_on_Quit_pressed)
+	assert(err == OK, "failed to connect to signal")
+
+	%Play.grab_focus()
+
+
+# -- SIGNAL HANDLERS ----------------------------------------------------------------- #
+
+
+func _on_Quit_pressed():
+	Lifecycle.shutdown(self)
