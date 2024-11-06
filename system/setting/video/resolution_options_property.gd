cb8b39f26312fb4ae5c70f0d131e4dd4ddd5ff56
coffeebeats <108542400+coffeebeats@users.noreply.github.com>
2024-11-06 13:19:20 -0800

    feat(setting): define properties for window resolution

---
+++ system/setting/video/resolution_options_property.gd
@@ -0,0 +1,16 @@
+##
+## system/setting/property/video/resolution_options.gd
+##
+## `VideoResolutionOptions` is a read-only settings property that provides a list of
+## display resolution options based on various video preferences.
+##
+
+extends StdSettingsPropertyVector2List
+
+# -- PRIVATE METHODS (OVERRIDES) ----------------------------------------------------- #
+
+
+func _get_value_from_config(_config: Config) -> Variant:
+	var resolutions := PackedVector2Array([Vector2(1920, 1080)])
+
+	return resolutions
