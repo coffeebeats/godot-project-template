879432d8a30eafef01c6c1a7d0cc6767934a2857
coffeebeats <108542400+coffeebeats@users.noreply.github.com>
2024-11-20 21:11:54 -0800

    fix(settings): add correct vsync property to sync with settings

---
+++ system/setting/video/vsync_property.gd
@@ -0,0 +1,28 @@
+##
+## system/setting/property/video/vsync_property.gd
+##
+## `VsyncModeProperty` is a boolean settings property that stores configuration values
+## as an integer `VSyncMode`.
+##
+
+extends StdSettingsPropertyBool
+
+# -- PRIVATE METHODS (OVERRIDES) ----------------------------------------------------- #
+
+func _get_value_from_config(config: Config) -> Variant:
+	var mode := config.get_int(category, name, _get_vsync_mode(default))
+
+	return mode == DisplayServer.VSYNC_ADAPTIVE
+
+func _set_value_on_config(config: Config, value: bool) -> bool:
+	print("SET ", value)
+	if value == default:
+
+		return config.erase(category, name)
+
+	return config.set_int(category, name, _get_vsync_mode(value))
+
+# -- PRIVATE METHODS ----------------------------------------------------------------- #
+
+func _get_vsync_mode(value: bool) -> DisplayServer.VSyncMode:
+	return DisplayServer.VSYNC_ADAPTIVE if value else DisplayServer.VSYNC_DISABLED
