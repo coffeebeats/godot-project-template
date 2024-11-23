29d9e9c82b9c5ab312bfd83a2be7c738ab65e240
coffeebeats <108542400+coffeebeats@users.noreply.github.com>
2024-11-23 15:50:38 -0800

    fix(settings): update `OptionButton` checkable style

--- project/settings/input/option_button.gd
+++ project/settings/input/option_button.gd
@@ -15,7 +15,13 @@ extends OptionButton
 ## radio_checkable controls whether the available options contain a radio button node.
 @export var radio_checkable: bool = false
 
+
 # -- ENGINE METHODS (OVERRIDES) ------------------------------------------------------ #
+
+func _exit_tree() -> void:
+	var popup_menu := get_popup()
+	if popup_menu.menu_changed.is_connected(_on_PopupMenu_menu_changed):
+		popup_menu.menu_changed.disconnect(_on_PopupMenu_menu_changed)
 
 
 func _ready():
@@ -24,8 +30,26 @@ func _ready():
 		"invalid state: cannot set checkable and radio_checkable",
 	)
 
+	_style_items()
+
+	var err := get_popup().menu_changed.connect(_on_PopupMenu_menu_changed)
+	assert(err == OK, "failed to connect to signal")
+
+
+# -- PRIVATE METHODS ----------------------------------------------------------------- #
+
+func _style_items() -> void:
 	var popup_menu := get_popup()
 
 	for i in popup_menu.get_item_count():
-		popup_menu.set_item_as_radio_checkable(i, radio_checkable)
-		popup_menu.set_item_as_checkable(i, checkable)
+		var is_radio_checkable := popup_menu.is_item_radio_checkable(i)
+
+		if radio_checkable and not is_radio_checkable:
+			popup_menu.set_item_as_radio_checkable(i, radio_checkable)
+		elif checkable and (not popup_menu.is_item_checkable(i) or is_radio_checkable):
+			popup_menu.set_item_as_checkable(i, checkable)
+
+# -- SIGNAL HANDLERS ----------------------------------------------------------------- #
+
+func _on_PopupMenu_menu_changed() -> void:
+	_style_items()
