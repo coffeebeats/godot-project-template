3366198691c29e348b514b24fee903cf5fc6e6ab
coffeebeats <108542400+coffeebeats@users.noreply.github.com>
2024-11-24 15:44:08 -0800

    fix(settings): improve resolution options based on actual safe display area

--- system/setting/video/resolution_options_property.gd
+++ system/setting/video/resolution_options_property.gd
@@ -12,9 +12,9 @@ extends StdSettingsPropertyVector2List
 ## window_mode_property is a settings property defining the window mode.
 @export var window_mode_property: StdSettingsPropertyInt = null
 
-## use_viewport_aspect_when_windowed controls whether the viewport's aspect ratio is
+## include_viewport_aspect_when_windowed controls whether the viewport's aspect ratio is
 ## used instead of the screen's when in non-fullscreen windowed mode.
-@export var use_viewport_aspect_when_windowed: bool = true
+@export var include_viewport_aspect_when_windowed: bool = true
 
 # -- INITIALIZATION ------------------------------------------------------------------ #
 
@@ -37,8 +37,8 @@ static var resolutions_5_4 := PackedVector2Array(
 static var resolutions_14_9 := PackedVector2Array(
 	[
 		# 14:9
-		Vector2(3024, 1964),  # MacBook Pro 14"
-		Vector2(3456, 2234),  # MacBook Pro 16"
+		Vector2(3024, 1964), # MacBook Pro 14"
+		Vector2(3456, 2234), # MacBook Pro 16"
 	]
 )
 
@@ -53,9 +53,9 @@ static var resolutions_16_9 := PackedVector2Array(
 		Vector2(2048, 1152),
 		Vector2(2560, 1440),
 		Vector2(3840, 2160),
-		Vector2(4096, 2304),  # iMac Retina 21.5"
-		Vector2(4480, 2520),  # iMac Retina 24"
-		Vector2(5120, 2880),  # iMac Retina 27"
+		Vector2(4096, 2304), # iMac Retina 21.5"
+		Vector2(4480, 2520), # iMac Retina 24"
+		Vector2(5120, 2880), # iMac Retina 27"
 		Vector2(7680, 4320),
 	]
 )
@@ -70,10 +70,10 @@ static var resolutions_16_10 := PackedVector2Array(
 		Vector2(1920, 1200),
 		Vector2(2048, 1280),
 		Vector2(2304, 1440),
-		Vector2(2560, 1664),  # Macbook Air 13"
+		Vector2(2560, 1664), # Macbook Air 13"
 		Vector2(2560, 1600),
 		Vector2(2880, 1800),
-		Vector2(2880, 1864),  # Macbook Air 15"
+		Vector2(2880, 1864), # Macbook Air 15"
 		Vector2(3024, 1890),
 		Vector2(3072, 1920),
 		Vector2(3456, 2160),
@@ -113,6 +113,8 @@ func _get_value_from_config(_config: Config) -> Variant:
 		return out
 
 	var screen := DisplayServer.get_display_safe_area().size
+	var title := Vector2i(0, DisplayServer.window_get_title_size("").y)
+
 	var viewport := Vector2(
 		ProjectSettings.get_setting_with_override(
 			&"display/window/size/viewport_width"
@@ -122,50 +124,45 @@ func _get_value_from_config(_config: Config) -> Variant:
 		),
 	)
 
-	var has_screen_resolution: bool = false
-	var target_aspect := (
-		viewport.aspect() if use_viewport_aspect_when_windowed else screen.aspect()
-	)
+	var size_max = (screen - title)
+
+	var target_aspects := [screen.aspect()]
+	if include_viewport_aspect_when_windowed:
+		target_aspects.append(viewport.aspect())
 
 	for resolutions in resolutions_by_aspect:
 		var is_matching_aspect := false
 
 		for resolution in resolutions:
-			if resolution.aspect() == target_aspect:
-				is_matching_aspect = true
-				break
+			for target_aspect in target_aspects:
+				if abs(target_aspect - resolution.aspect()) <= 0.02:
+					is_matching_aspect = true
+					break
 
 		if not is_matching_aspect:
 			continue
 
-		var to_add: PackedVector2Array = resolutions
-		if resolutions == resolutions_14_9:
-			to_add = resolutions.duplicate()
-			to_add.append_array(resolutions_16_10)
-
-		for resolution in to_add:
-			if resolution.x > screen.x or resolution.y > screen.y:
+		for resolution in resolutions:
+			if resolution.x > size_max.x or resolution.y > size_max.y:
 				continue
 
 			out.append(resolution)
-
-			var current := Vector2i(round(resolution.x), round(resolution.y))
-			if current == screen:
-				has_screen_resolution = true
 
 	# If no resolutions match the screen's aspect ratio, just provide all resolutions
 	# smaller than the screen and let the user decide.
 	if out.is_empty():
 		for resolutions in resolutions_by_aspect:
 			for resolution in resolutions:
-				if resolution.x > screen.x or resolution.y > screen.y:
+				if resolution.x > size_max.x or resolution.y > size_max.y:
 					continue
 
 				out.append(resolution)
 
-	if not has_screen_resolution:
-		out.append(screen)
-
 	out.sort()
 
 	return out
+
+# -- PRIVATE METHODS ----------------------------------------------------------------- #
+
+func _round_to_vector2i(value: Vector2) -> Vector2i:
+	return Vector2i(round(value.x), round(value.y))
