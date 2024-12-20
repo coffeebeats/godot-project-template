5b9aa8a58a16511f4c15f1c00e5c11b310eb97dd
coffeebeats <108542400+coffeebeats@users.noreply.github.com>
2024-12-19 17:31:07 -0800

    feat(ui): implement ability to only show one KBM or joypad glyphs

--- ui/glyph/glyph.gd
+++ ui/glyph/glyph.gd
@@ -9,27 +9,60 @@
 @tool
 extends StdInputGlyph
 
+# -- CONFIGURATION ------------------------------------------------------------------- #
+
+@export_group("Display")
+
+@export_subgroup("Visibility")
+
+## show_on_kbm controls whether this glyph is displayed when the active device is a
+## keyboard and mouse.
+@export var show_on_kbm: bool = true
+
+## show_on_joy controls whether this glyph is displayed when the active device is a
+## joypad.
+@export var show_on_joy: bool = true
+
 # -- INITIALIZATION ------------------------------------------------------------------ #
+
+var _is_device_compatible: bool = true
 
 @onready var _label: Label = get_node("Label")
 @onready var _texture_rect: TextureRect = get_node("TextureRect")
 
+# -- ENGINE METHODS (OVERRIDES) ------------------------------------------------------ #
+
+
+func _ready() -> void:
+	_device_activated(_slot)
+	super._ready()
+
+
 # -- PRIVATE METHODS (OVERRIDES) ----------------------------------------------------- #
+
+
+func _device_activated(device: StdInputDevice) -> void:
+	_is_device_compatible = (
+		show_on_kbm if device.device_type == StdInputDevice.DEVICE_TYPE_KEYBOARD else show_on_joy
+	)
+
 
 func _update_glyph() -> bool:
 	var texture_prev: Texture = _texture_rect.texture
 	var label_prev: String = _label.text
 
-	_texture_rect.texture = _slot.get_action_glyph(action_set.name, action, _texture_rect.size)
+	_texture_rect.texture = null
+	_label.text = ""
+
+	if _is_device_compatible:
+		_texture_rect.texture = _slot.get_action_glyph(action_set.name, action, _texture_rect.size)
+		_label.text = (
+			_slot.get_action_origin_label(action_set.name, action)
+			if not _texture_rect.texture
+			else ""
+		)
+
 	_texture_rect.visible = _texture_rect.texture != null
-
-	_label.text = (
-		_slot.get_action_origin_label(action_set.name, action)
-		if not _texture_rect.texture
-		else ""
-	)
-
-	_label.text = _label.text.to_upper()
 	_label.visible = _label.text != ""
 
 	return _texture_rect.texture != texture_prev or _label.text != label_prev
