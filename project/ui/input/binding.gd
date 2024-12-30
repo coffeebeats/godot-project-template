7302034d91dc3cac445b5b769b14efffa983490f
coffeebeats <108542400+coffeebeats@users.noreply.github.com>
2024-12-30 13:29:31 -0600

    fix(project): pass bindings scope to `Binding` input

--- project/ui/input/binding.gd
+++ project/ui/input/binding.gd
@@ -16,20 +16,51 @@ const Rebinder := preload("res://project/settings/rebinder.gd")
 
 @export_group("Binding")
 
+## scope is the settings scope in which binding overrides will be stored.
+@export var scope: StdSettingsScope = null:
+	set(value):
+		scope = value
+
+		if _glyph:
+			_glyph.scope = value
+
 @export_subgroup("Action")
 
 ## action_set is an input action set which defines the configured action.
-@export var action_set: StdInputActionSet = null
+@export var action_set: StdInputActionSet = null:
+	set(value):
+		action_set = value
+
+		if _glyph:
+			_glyph.action_set = value
 
 ## action is the name of the input action which the glyph icon will correspond to.
-@export var action := &""
+@export var action := &"":
+	set(value):
+		action = value
+
+		if _glyph:
+			_glyph.action = value
+
+## binding_index is the index/rank (e.g. primary or secondary) of the action binding.
+@export var binding_index: int = 0:
+	set(value):
+		binding_index = value
+
+		if _glyph:
+			_glyph.binding_index = value
 
 @export_subgroup("Player")
 
 ## player_id is a player identifier which will be used to look up the action's input
 ## origin bindings. Specifically, this is used to find the corresponding `StdInputSlot`
 ## node, which must be present in the scene tree.
-@export var player_id: int = 1
+@export var player_id: int = 1:
+	set(value):
+		player_id = value
+
+		if _glyph:
+			_glyph.player_id = value
 
 # -- INITIALIZATION ------------------------------------------------------------------ #
 
@@ -50,8 +81,9 @@ func _get_configuration_warnings() -> PackedStringArray:
 	return _glyph._get_configuration_warnings()
 
 
-func _ready():
+func _ready() -> void:
 	assert(_glyph is StdInputGlyph, "invalid state; missing node")
+	_glyph.scope = scope
 	_glyph.action_set = action_set
 	_glyph.action = action
 	_glyph.player_id = player_id
@@ -71,4 +103,4 @@ func _on_glyph_updated(_has_contents: bool) -> void:
 
 func _on_pressed() -> void:
 	var binding_prompt: Rebinder = Rebinder.find_in_scene()
-	binding_prompt.start(action_set, action, player_id)
+	binding_prompt.start(action_set, action, binding_index, player_id)
