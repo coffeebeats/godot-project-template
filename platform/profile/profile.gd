3f5b13f54a2e99007d9bf344b1000a166ccd38c9
coffeebeats <108542400+coffeebeats@users.noreply.github.com>
2025-01-06 18:09:44 -0800

    feat(platform): make `Profile` available via `Platform`

---
+++ platform/profile/profile.gd
@@ -0,0 +1,29 @@
+##
+## platform/profile/profile.gd
+##
+## Profile is a `Platform` node which manages information about the user running the
+## game application.
+##
+
+extends Node
+
+# -- DEFINITIONS --------------------------------------------------------------------- #
+
+# -- INITIALIZATION ------------------------------------------------------------------ #
+
+var _profile: UserProfile = null
+
+# -- PUBLIC METHODS ------------------------------------------------------------------ #
+
+## get_user_profile returns information about the profile currently running the game
+## application.
+func get_user_profile() -> UserProfile:
+	return _profile
+
+## set_user_profile updates the current user profile.
+func set_user_profile(profile: UserProfile) -> void:
+	if _profile:
+		assert(false, "invalid state; user profile already set")
+		return
+	
+	_profile = profile
