fa320798f2e92fea51fef8a13358d288167766d4
coffeebeats <108542400+coffeebeats@users.noreply.github.com>
2025-01-22 15:08:40 -0800

    fix(platform): make `Profile` filepath computation public

--- platform/profile/sync_target.gd
+++ platform/profile/sync_target.gd
@@ -15,21 +15,30 @@ extends StdSettingsSyncTarget
 ## the 'StdSettingsScope' contents will be synced to.
 @export var path: String = ""
 
+# -- PUBLIC METHODS ------------------------------------------------------------------ #
+
+
+## make_path_relative_to_profile returns an absolute path to provided profile-relative
+## path.
+static func make_path_relative_to_profile(path_rel: String) -> String:
+	path_rel = path_rel.trim_prefix("profile://")
+
+	if path_rel.is_absolute_path():
+		assert(false, "invalid config; expected relative path")
+		return ""
+
+	var profile := Platform.get_user_profile()
+	if not profile:
+		return ""
+
+	return ("user://profiles/%s" % profile.id).path_join(path_rel)
+
+
 # -- PRIVATE METHODS (OVERRIDES) ----------------------------------------------------- #
 
 
 func _create_sync_target_node() -> StdConfigWriter:
-	path = path.trim_prefix("profile://")
-
-	if path.is_absolute_path():
-		assert(false, "invalid config; expected relative path")
-		return null
-
-	var profile := Platform.get_user_profile()
-	if not profile:
-		return
-
 	var writer := StdConfigWriterBinary.new()
-	writer.path = ("user://profiles/%s" % profile.id).path_join(path)
+	writer.path = make_path_relative_to_profile(path)
 
 	return writer
