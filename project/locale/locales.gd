0168e34f45b68e48e996fc45fff9f515e63a3fa9
coffeebeats <108542400+coffeebeats@users.noreply.github.com>
2025-01-27 17:40:28 -0800

    fix(project): localize Steam Input actions manifest; centralize input action translation (#226)
    * chore(addons): update `std` to `v1.14.3`
    
    * feat(project): create a shared library for translating input actions
    
    * refactor(project): use shared input action translation implementation in settings menu
    
    * feat(project): add translations to Steam Input manifest
    
    * chore: fix formatting (on behalf of 'coffeebeats')
    
    * fix(project): default to using source message for translated actions
    
    * chore: fix formatting (on behalf of 'coffeebeats')
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ project/locale/locales.gd
@@ -0,0 +1,64 @@
+##
+## project/locale/locales.gd
+##
+## A shared library for working with translations.
+##
+## NOTE: This 'Object' should *not* be instanced and/or added to the 'SceneTree'. It is a
+## "static" library that can be imported at compile-time using 'preload'.
+##
+
+extends Object
+
+# -- DEFINITIONS --------------------------------------------------------------------- #
+
+const MSGID_ACTION_PREFIX := &"actions_"
+const MSGID_ACTION_SET_PREFIX := &"options_controls_"
+
+# -- PUBLIC METHODS ------------------------------------------------------------------ #
+
+
+## tr_action translates an input action. Provide `locale` to specify a locale other than
+## the one currently loaded.
+static func tr_action(
+	action_set: StringName,
+	action: StringName,
+	locale: StringName = &"",
+) -> String:
+	var translated := _translate(action, MSGID_ACTION_PREFIX + action_set, locale)
+	return translated if translated else str(action)
+
+
+## tr_action_set translates an input action set. Provide `locale` to specify a locale
+## other than the one currently loaded.
+static func tr_action_set(action_set: StringName, locale: StringName = &"") -> String:
+	var translated := _translate(MSGID_ACTION_SET_PREFIX + action_set, &"", locale)
+	return translated if translated else str(action_set)
+
+
+# -- PRIVATE METHODS ----------------------------------------------------------------- #
+
+
+static func _translate(
+	msg: StringName,
+	ctx: StringName = &"",
+	locale: StringName = &"",
+) -> String:
+	if locale == &"":
+		locale = TranslationServer.get_locale()
+
+	var translation := TranslationServer.get_translation_object(locale)
+	if not translation:
+		assert(false, "invalid argument; missing translation")
+		return msg
+
+	return translation.get_message(msg, ctx)
+
+
+# -- ENGINE METHODS (OVERRIDES) ------------------------------------------------------ #
+
+
+func _init():
+	assert(
+		not OS.is_debug_build(),
+		"Invalid config; this 'Object' should not be instantiated!"
+	)
