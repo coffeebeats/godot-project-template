45c8d0dbc098b02f6b2fbde145b15b0581777872
coffeebeats <108542400+coffeebeats@users.noreply.github.com>
2025-01-29 08:16:52 -0800

    wip

--- project/main/menu/main_menu.gd
+++ project/main/menu/main_menu.gd
@@ -16,11 +16,13 @@ const Credits := preload("../credit/credits.tscn")
 
 func _ready() -> void:
 	Signals.connect_safe(%Quit.pressed, _on_quit_pressed)
-	Signals.connect_safe($Credits.pressed, _on_credits_pressed)
+	Signals.connect_safe(%Options.pressed, _on_options_pressed)
 	Signals.connect_safe(%Play.pressed, _on_play_pressed)
+	Signals.connect_safe(%Credits.pressed, _on_credits_pressed)
+	Signals.connect_safe($Credits.opened, _on_credits_changed)
+	Signals.connect_safe($Credits.closed, _on_credits_changed)
 	Signals.connect_safe($SaveSlots.opened, _on_save_slots_changed)
 	Signals.connect_safe($SaveSlots.closed, _on_save_slots_changed)
-	Signals.connect_safe(%Options.pressed, _on_options_pressed)
 	Signals.connect_safe($Settings.closed, _on_settings_changed)
 	Signals.connect_safe($Settings.opened, _on_settings_changed)
 
@@ -42,6 +44,12 @@ func _on_play_pressed() -> void:
 
 func _on_quit_pressed() -> void:
 	Lifecycle.shutdown()
+
+
+func _on_credits_changed() -> void:
+	# TODO: Replace this with proper navigation stack.
+	if $Credits.visible:
+		get_viewport().gui_release_focus()
 
 
 func _on_save_slots_changed() -> void:
