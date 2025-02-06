7f96438fdd4f0de879a4a8356167148ff5ec0188
coffeebeats <108542400+coffeebeats@users.noreply.github.com>
2025-02-06 09:35:06 -0800

    fix(system): add documentation/comments

--- system/system.gd
+++ system/system.gd
@@ -1,7 +1,10 @@
 ##
 ## system/system.gd
 ##
-## A shared library for ...
+## Systems is a shim for the `System` autoload scene which allows accessing its
+## components without directly referencing the autoloaded scene itself. This is a
+## workaround for errors encountered when scripts referencing the `Systems` autoload are
+## loaded in the background (see https://github.com/godotengine/godot/issues/98865).
 ##
 ## NOTE: This 'Object' should *not* be instanced and/or added to the 'SceneTree'. It is a
 ## "static" library that can be imported at compile-time using 'preload'.
@@ -17,9 +20,11 @@ const SavesSystem := preload("res://system/save/saves.gd")
 
 # -- PUBLIC METHODS ------------------------------------------------------------------ #
 
+## input returns the `Input` system component.
 static func input() -> InputSystem:
 	return StdGroup.get_sole_member(InputSystem.GROUP_INPUT_SHIM)
 
+## saves returns the `Saves` system component.
 static func saves() -> SavesSystem:
 	return StdGroup.get_sole_member(SavesSystem.GROUP_SAVES_SHIM)
 
