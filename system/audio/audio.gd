338ebbf07970e107248d6f65e184913d3949f540
coffeebeats <108542400+coffeebeats@users.noreply.github.com>
2025-02-09 21:42:35 -0800

    feat(system): create an `Audio` system component using `std` types (#256)
    * chore(addons): update `std` to `v1.16.0`
    
    * chore(system): create `StdAudioBus` resources for all four predefined audio buses
    
    * feat(system): add a cooldown to ui navigation actions
    
    * feat(system): create an `Audio` system component
    
    * feat(system): create a reference to the new audio system component
    
    * chore(platform,system): remove unused components
    
    * chore: fix formatting (on behalf of 'coffeebeats')
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- system/audio/audio.gd
+++ system/audio/audio.gd
@@ -1,5 +1,26 @@
 ##
-## Insert class description here.
+## system/audio/audio.gd
+##
+## SystemAudio is a sound event player which manages pools of audio player nodes.
 ##
 
-extends Node
+extends StdSoundEventPlayer
+
+# -- DEFINITIONS --------------------------------------------------------------------- #
+
+const GROUP_AUDIO_SHIM := &"system/audio:shim"
+
+# -- ENGINE METHODS (OVERRIDES) ------------------------------------------------------ #
+
+
+func _enter_tree() -> void:
+	super._enter_tree()
+
+	assert(StdGroup.is_empty(GROUP_AUDIO_SHIM), "invalid state; duplicate node found")
+	StdGroup.with_id(GROUP_AUDIO_SHIM).add_member(self)
+
+
+func _exit_tree() -> void:
+	super._exit_tree()
+
+	StdGroup.with_id(GROUP_AUDIO_SHIM).remove_member(self)
