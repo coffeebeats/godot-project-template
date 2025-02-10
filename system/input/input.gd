4430115da44bc2e1b70f72db065624e51b0e6064
coffeebeats <108542400+coffeebeats@users.noreply.github.com>
2025-02-09 18:36:50 -0800

    feat(system): add a cooldown to ui navigation actions

--- system/input/input.gd
+++ system/input/input.gd
@@ -8,6 +8,10 @@ extends Node
 
 # -- SIGNALS ------------------------------------------------------------------------- #
 
+## cursor_visibility_changed is emitted when the visibility of the application's cursor
+## changes.
+signal cursor_visibility_changed(visible: bool)
+
 ## focus_root_changed is emitted when the focus root changes. Note that `root` may be
 ## `null` if the root focus was cleared.
 signal focus_root_changed(root: Control)
@@ -25,11 +29,21 @@ const GROUP_INPUT_SHIM := &"system/input:shim"
 ## action_set_default is a default action set that will be loaded after starting up.
 @export var action_set_default: StdInputActionSet = null
 
+## ui_navigation_cooldown is a wait period after inputting a UI navigation action that
+## must elapse prior to another one being accepted.
+@export var ui_navigation_cooldown: float = 0.08
+
 # -- INITIALIZATION ------------------------------------------------------------------ #
 
 var _cursor: StdInputCursor = null
+var _ui_navigation_cooldown: float = 0.0
 
 # -- PUBLIC METHODS ------------------------------------------------------------------ #
+
+## is_cursor_visible returns whether the cursor is currently visible. This can be used
+## to check whether focus-based navigation is in effect.
+func is_cursor_visible() -> bool:
+	return _cursor.get_is_visible()
 
 
 ## is_using_focus_ui_navigation returns whether the game is currently using focus-based
@@ -51,10 +65,38 @@ func _enter_tree() -> void:
 	assert(StdGroup.is_empty(GROUP_INPUT_SHIM), "invalid state; duplicate node found")
 	StdGroup.with_id(GROUP_INPUT_SHIM).add_member(self)
 
+	set_process(false)
+
 
 func _exit_tree() -> void:
 	StdGroup.with_id(GROUP_INPUT_SHIM).remove_member(self)
 
+func _input(event: InputEvent) -> void:
+	if not event.is_action_type():
+		return
+
+	if (
+		event.is_action_pressed(&"ui_up", true)
+		or event.is_action_pressed(&"ui_right", true)
+		or event.is_action_pressed(&"ui_down", true)
+		or event.is_action_pressed(&"ui_left", true)
+	):
+		if is_processing():
+			get_viewport().set_input_as_handled()
+			return
+
+		_ui_navigation_cooldown = ui_navigation_cooldown
+		set_process(true)
+
+
+func _process(delta: float) -> void:
+	# Try to normalize the rate of UI inputs by treating anything below one frame
+	# as successfully completed.
+	if _ui_navigation_cooldown < (1.0 / Engine.get_frames_per_second() / 2.0):
+		set_process(false)
+		return
+
+	_ui_navigation_cooldown -= delta
 
 func _ready() -> void:
 	assert(action_set_default is StdInputActionSet, "invalid state; missing action set")
@@ -62,7 +104,8 @@ func _ready() -> void:
 	_cursor = StdGroup.get_sole_member(StdInputCursor.GROUP_INPUT_CURSOR)
 	assert(_cursor is StdInputCursor, "invalid state; missing input cursor")
 
-	# Forward the focus root change events.
+	# Forward the `StdInputCursor` events.
+	Signals.connect_safe(_cursor.cursor_visibility_changed, cursor_visibility_changed.emit)
 	Signals.connect_safe(_cursor.focus_root_changed, focus_root_changed.emit)
 
 	for slot in StdInputSlot.all():
