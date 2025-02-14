cbe9a85a11cd3cfcd4c1e9574f2ea226b7c2cb37
coffeebeats <108542400+coffeebeats@users.noreply.github.com>
2025-02-14 12:08:39 -0800

    fix(project): polish sound effects and input handling in main menu scene

--- project/main/menu/main_menu.gd
+++ project/main/menu/main_menu.gd
@@ -19,6 +19,9 @@ const Signals := preload("res://addons/std/event/signal.gd")
 ## the intensity of main menu music when the settings menu is open.
 @export var music_filter_param: StdSoundParamAudioEffect = null
 
+## toggle_settings_action_prompt is an optional action prompt to open the settings menu.
+@export var toggle_settings_action_prompt: InputActionPrompt = null
+
 # -- INITIALIZATION ------------------------------------------------------------------ #
 
 var _music_sound_instance: StdSoundInstance = null
@@ -43,8 +46,7 @@ func _input(event: InputEvent) -> void:
 		return
 
 	if event.is_action_pressed(&"ui_toggle_settings"):
-		# NOTE: Don't handle this so that `Input` processors can see the action and hide
-		# the cursor, for example.
+		get_viewport().set_input_as_handled()
 
 		if not Modal.are_any_open():
 			_settings.visible = true
@@ -53,6 +55,20 @@ func _input(event: InputEvent) -> void:
 
 
 func _ready() -> void:
+	# Move to front of parent's children so system components see events first.
+	get_viewport().move_child.call_deferred(self, 0)
+
+	if toggle_settings_action_prompt:
+		(
+			Signals
+			. connect_safe(
+				toggle_settings_action_prompt.pressed,
+				_on_settings_prompt_pressed,
+			)
+		)
+
+	_handle_first_focused_sound_event_mute.call_deferred()
+
 	Signals.connect_safe(_options.pressed, _on_options_pressed)
 	Signals.connect_safe(_play.pressed, _on_play_pressed)
 	Signals.connect_safe(_quit.pressed, _on_quit_pressed)
@@ -66,15 +82,32 @@ func _ready() -> void:
 		_music_sound_instance = audio.play(music_sound_event, 2, Tween.TRANS_LINEAR)
 
 
+# -- PRIVATE METHODS ----------------------------------------------------------------- #
+
+
+func _handle_first_focused_sound_event_mute() -> void:
+	var input := Systems.input()
+	if not input.is_cursor_visible():
+		input.mute_next_focus_sound_event()
+
+
 # -- SIGNAL HANDLERS ----------------------------------------------------------------- #
 
 
 func _on_modal_closed(_reason: Modal.CloseReason) -> void:
+	var input := Systems.input()
+	if not input.is_cursor_visible():
+		input.mute_next_focus_sound_event()
+
 	if music_filter_param:
 		music_filter_param.enabled = false
 
 
 func _on_modal_opened() -> void:
+	var input := Systems.input()
+	if not input.is_cursor_visible():
+		input.mute_next_focus_sound_event()
+
 	if music_filter_param:
 		music_filter_param.enabled = true
 
@@ -85,6 +118,10 @@ func _on_options_pressed() -> void:
 
 func _on_play_pressed() -> void:
 	_saves.visible = true
+
+
+func _on_settings_prompt_pressed() -> void:
+	_settings.visible = true
 
 
 func _on_quit_pressed() -> void:
