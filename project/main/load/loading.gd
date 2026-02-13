993147ef07e8a2c04a6aa4bbf27b76d46ead3725
coffeebeats <108542400+coffeebeats@users.noreply.github.com>
2026-02-12 17:34:59 -0800

    refactor(project): streamline loading screen

--- project/main/load/loading.gd
+++ project/main/load/loading.gd
@@ -1,48 +1,35 @@
 ##
 ## project/main/load/loading.gd
 ##
-## Loading implements a loading screen scene.
+## Loading implements a loading screen scene. It emits a signal when loading completes
+## with the result.
 ##
 
 extends Control
 
-# -- DEPENDENCIES -------------------------------------------------------------------- #
+# -- SIGNALS ------------------------------------------------------------------------- #
 
-const AdvanceEvent := preload("res://addons/std/scene/event/advance.gd")
-const SceneHandle := preload("res://addons/std/scene/handle.gd")
-
-# -- CONFIGURATION ------------------------------------------------------------------- #
-
-## scene_handle is a node path to a 'godot-plugin-std' scene handle used to emit
-## transition events to the main scene state machine.
-@export var scene_handle: SceneHandle = null
-
-## scene_path_failed is the scene state to navigate to if loading fails.
-@export var scene_path_failed: NodePath = ^"Main/Transition"
-
-## scene_path_success is the scene state to navigate to if loading succeeds.
-@export var scene_path_success: NodePath = ^"Core/Map/Transition"
+## finished is emitted when loading finishes.
+signal finished(success: bool, data)
 
 # -- ENGINE METHODS (OVERRIDES) ------------------------------------------------------ #
 
 
 func _ready() -> void:
-	assert(scene_handle is SceneHandle, "invalid config; missing scene handle")
-
-	_load_scene()
+	await _load_save_data()
 
 
 # -- PRIVATE METHODS ----------------------------------------------------------------- #
 
 
-func _load_scene() -> void:
+func _load_save_data() -> void:
 	var saves := Systems.saves()
 	var data := saves.create_new_save_data()
 
 	if not await saves.load_save_data(data):
-		scene_handle.transition_to(scene_path_failed)
+		finished.emit(false, null)
 		return
 
 	# TODO: Add behavior to load the necessary resources and set game system state.
 
-	scene_handle.transition_to(scene_path_success, data)
+	finished.emit(true, data)
