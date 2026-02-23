f8bbf3ad7a11297b6a909cb88795f114f29492c7
coffeebeats <108542400+coffeebeats@users.noreply.github.com>
2026-02-23 09:10:16 -0800

    refactor(project/save): update save slot menu to use new menu primitives

--- project/save/menu.gd
+++ project/save/menu.gd
@@ -9,6 +9,9 @@ extends PanelContainer
 
 # -- DEPENDENCIES -------------------------------------------------------------------- #
 
+const ConfirmScene := preload("res://project/save/confirm.tscn")
+const ConfirmScreen := preload("res://project/save/confirm_screen.tres")
+const Signals := preload("res://addons/std/event/signal.gd")
 const SlotButton := preload("slot_button.gd")
 
 # -- INITIALIZATION ------------------------------------------------------------------ #
@@ -19,9 +22,7 @@ const SlotButton := preload("slot_button.gd")
 # -- ENGINE METHODS (OVERRIDES) ------------------------------------------------------ #
 
 
-func _ready():
-	$Close.pressed.connect(_on_close_pressed)
-
+func _ready() -> void:
 	for child in _slot_buttons.get_children():
 		var button: SlotButton = child
 		if not button is SlotButton:
@@ -36,15 +37,12 @@ func _ready():
 		if not button is Button:
 			continue
 
-		button.disabled = saves.get_save_slot(slot).status == SaveSlot.STATUS_EMPTY
+		var status := saves.get_save_slot(slot).status
+		button.disabled = status == SaveSlot.STATUS_EMPTY
 		button.pressed.connect(_on_delete_button_pressed.bind(button, slot))
 
 
 # -- SIGNAL HANDLERS ----------------------------------------------------------------- #
-
-
-func _on_close_pressed() -> void:
-	StdInputEvent.trigger_action(&"ui_cancel")
 
 
 func _on_delete_button_pressed(button: Button, slot: int) -> void:
@@ -52,10 +50,24 @@ func _on_delete_button_pressed(button: Button, slot: int) -> void:
 	if saves.get_save_slot(slot).status == SaveSlot.STATUS_EMPTY:
 		return
 
-	button.disabled = true
+	var scene := ConfirmScene.instantiate()
+	var dialog: PanelContainer = scene.get_node("%AlertDialog")
 
-	if not saves.erase_slot(slot):
-		button.disabled = false
+	dialog.confirmed.connect(
+		func():
+			Main.screens().pop()
+			button.disabled = true
+			if not saves.erase_slot(slot):
+				button.disabled = false,
+		CONNECT_ONE_SHOT,
+	)
+
+	dialog.cancelled.connect(
+		func(): Main.screens().pop(),
+		CONNECT_ONE_SHOT,
+	)
+
+	Main.screens().push(ConfirmScreen, scene)
 
 
 func _on_slot_button_pressed(slot: int) -> void:
