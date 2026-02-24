0576e9a958d0fcf9053240b2a4a74da1efca4bab
coffeebeats <108542400+coffeebeats@users.noreply.github.com>
2026-02-23 18:57:10 -0800

    chore(project): move project-level menus into `project/menu` directory (#398)
    * chore(project): reorganize save, pause, and settings menus
    
    * fix(project/menu): fix rebinder bug

---
+++ project/menu/save/menu.gd
@@ -0,0 +1,71 @@
+##
+## project/menu/save/menu.gd
+##
+## SaveSlots is a menu which displays save slot information and, upon selecting one,
+## proceeds to load the game scene.
+##
+
+extends PanelContainer
+
+# -- DEPENDENCIES -------------------------------------------------------------------- #
+
+const ConfirmDelete := preload("res://project/menu/save/delete.tscn")
+const Signals := preload("res://addons/std/event/signal.gd")
+const SlotButton := preload("slot_button.gd")
+
+# -- INITIALIZATION ------------------------------------------------------------------ #
+
+@onready var _delete_buttons: Control = %DeleteButtons
+@onready var _slot_buttons: Control = %SlotButtons
+
+# -- ENGINE METHODS (OVERRIDES) ------------------------------------------------------ #
+
+
+func _ready() -> void:
+	for child in _slot_buttons.get_children():
+		var button: SlotButton = child
+		if not button is SlotButton:
+			continue
+
+		button.pressed.connect(_on_slot_button_pressed.bind(button.slot))
+
+	var saves := Systems.saves()
+
+	for slot in _delete_buttons.get_child_count():
+		var button: Button = _delete_buttons.get_child(slot)
+		if not button is Button:
+			continue
+
+		var status := saves.get_save_slot(slot).status
+		button.disabled = status == SaveSlot.STATUS_EMPTY
+		button.pressed.connect(_on_delete_button_pressed.bind(button, slot))
+
+
+# -- SIGNAL HANDLERS ----------------------------------------------------------------- #
+
+
+func _on_delete_button_pressed(button: Button, slot: int) -> void:
+	var saves := Systems.saves()
+	if saves.get_save_slot(slot).status == SaveSlot.STATUS_EMPTY:
+		return
+
+	var instance := ConfirmDelete.instantiate()
+	var dialog: AlertDialog = instance.get_node("%AlertDialog")
+
+	Signals.connect_safe(
+		dialog.closed,
+		func(accepted: bool):
+			Main.screens().pop()
+			if accepted:
+				button.disabled = true
+				if not saves.erase_slot(slot):
+					button.disabled = false,
+		CONNECT_ONE_SHOT,
+	)
+
+	Main.screens().push(StdScreen.new(), instance)
+
+
+func _on_slot_button_pressed(slot: int) -> void:
+	# TODO: Close the save menu on error.
+	Main.load_game(slot)
