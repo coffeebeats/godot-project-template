992d8fa6eead2fe820198174c4b90c6d37678d1b
coffeebeats <108542400+coffeebeats@users.noreply.github.com>
2026-03-08 14:26:11 -0700

    feat(project): create a base scene for 2D (vector) maps

---
+++ project/maps/base/2d/scene.gd
@@ -0,0 +1,9 @@
+##
+## project/maps/base/2d/scene.gd
+##
+## Base scene for standard 2D games. Renders the game world in a `SubViewport` at native
+## resolution with linear filtering.
+##
+
+@tool
+extends "res://project/maps/base/scene.gd"
